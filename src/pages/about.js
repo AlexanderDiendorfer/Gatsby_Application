a1497719fa51d5812c5a3a245e99d7bc86e0876e
Alexander Diendorfer+ <20122072@htlneufelden.onmicrosoft.comgit config --global user.email 20122072@htlneufelden.onmicrosoft.comgit config --global user.email 20122072@htlneufelden.onmicrosoft.com>
2019-05-17 12:05:37 +0200

    Initial

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      </p>
+    </div>
+  )
+}
+
+export default ContactPage
